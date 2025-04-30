8d08d0e42c5e12fbf94e019821d659363f4041eb
vladyslav <vladyslav.korniienko@stud.onu.edu.ua>
2025-04-30 20:55:25 +0300

    add answers for questions

---
+++ js/task-3.js
@@ -0,0 +1,32 @@
+class StringBuilder {
+  #initialValue;
+
+  constructor(params) {
+    this.#initialValue = params;
+  }
+
+  getValue() {
+    return this.#initialValue;
+  }
+
+  padEnd(str) {
+    this.#initialValue = this.#initialValue + str;
+  }
+
+  padStart(str) {
+    this.#initialValue = str + this.#initialValue;
+  }
+
+  padBoth(str) {
+    this.#initialValue = str + this.#initialValue + str;
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
